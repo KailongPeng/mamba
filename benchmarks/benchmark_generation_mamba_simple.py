5a041cb8607c785a4f10a2cdd36df43ec4b27f0f
KailongPeng <kailongpeng001@gmail.com>
2024-03-23 22:21:04 -0400

    understand

--- benchmarks/benchmark_generation_mamba_simple.py
+++ benchmarks/benchmark_generation_mamba_simple.py
@@ -47,11 +47,11 @@ from torchinfo import summary
 # 假设model已经被加载，并处于eval模式
 model.eval()
 
-# 针对这个示例，我们使用args.promptlen作为输入序列长度
-input_size = (args.batch, args.promptlen)
-
-# 打印模型概览
-summary(model, input_size=input_size, device=device)
+# # 针对这个示例，我们使用args.promptlen作为输入序列长度
+# input_size = (args.batch, args.promptlen)
+#
+# # 打印模型概览
+# summary(model, input_size=input_size, device=device)
 
 import pdb ; pdb.set_trace()
 
